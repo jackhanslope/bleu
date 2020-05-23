2435d828b37915c1e50b3ca9a5547180fbcb4220
Jack Hanslope <jackhansp@btinternet.com>
2020-05-23 17:15:23 +0100

    Refactor to have config struct and associated functions.

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,27 @@ use blurz::bluetooth_session::BluetoothSession as Session;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let command = &args[1];
-    let device = &args[2];
+    let config = Config::new(&args);
 
-    println!("Running {}", command);
-    println!("Device: {}", device);
+    println!("Command: {}", config.command);
+    println!("Device: {}", config.device);
 
     let session = &Session::create_session(None).unwrap();
     let adapter: Adapter = Adapter::init(session).unwrap();
     let device: Device = adapter.get_first_device().unwrap();
     println!("device: {:?}", device);
+}
+
+struct Config {
+    command: String,
+    device: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let command = args[1].clone();
+        let device = args[2].clone();
+
+        Config { command, device }
+    }
 }
