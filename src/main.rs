0a222044a37a3517ba22cb14e073dfd9755d05c4
Jack Hanslope <jackhansp@btinternet.com>
2020-05-28 00:22:42 +0100

    Update to work with clap. Seems to be working as before (almost) but with clap now.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ use clap::{App, Arg, SubCommand};
 use std::env;
 use std::process;
 
-use blue::Config;
-
 fn main() {
-    let matches = App::new("blue")
+    let app = App::new("blue")
         .version("0.1.0")
         .author("Jack Hanslope <jackhansp@btinternet.com>")
         .about("A bluetooth cli written in rust")
@@ -38,25 +36,10 @@ fn main() {
                         .index(1)
                         .conflicts_with("all"),
                 ),
-        )
-        .get_matches();
+        );
 
-    if let Some(ref matches) = matches.subcommand_matches("connect") {
-        println!("Connecting to {}", matches.value_of("device").unwrap());
+    if let Err(e) = blue::run(app) {
+        println!("{}", e);
+        process::exit(1);
     }
-
-    if let Some(ref matches) = matches.subcommand_matches("disconnect") {
-        if let Some(d) = matches.value_of("device") {
-            println!("Disconnecting from {}", d);
-        } else if matches.is_present("all") {
-            println!("Disconnecting from all");
-        } else {
-            println!("No arguments provided, disconnecting from all.");
-        }
-    }
-
-    // if let Err(e) = blue::run(config) {
-    // println!("{}", e);
-    // process::exit(1);
-    // }
 }
